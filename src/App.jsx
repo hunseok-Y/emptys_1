c3b57ded02648e420204be45a5b9d82a42c87db0
yanghoonseok <gnstjr5911@gmail.com>
2025-05-28 22:39:56 +0900

    Movies.jsx, NavMenus.jsx, Tvs.jsx 컴포넌트화 하기

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,15 @@
 import './App.css'
-import {useEffect, useState} from "react";
-import axios from "axios";
-import {Button, Card, Col, Container, Row} from "react-bootstrap";
+import NavMenus from "./NavMenus.jsx";
+import Tvs from "./Tvs.jsx";
+import Movies from "./Movies.jsx";
 
 function App() {
-
-    const [movies, setMovies] = useState([]);
-
-    const getMovies = async () => {
-        try {
-            const url = 'https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1';
-            const options = {
-                method: 'GET',
-                headers: {
-                    accept: 'application/json',
-                    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5Y2NlMjljMjc2MzZmODY0OGM0YzlmNmMyNGQyYTNhOCIsIm5iZiI6MTc0NzU0OTY5Ni41NjMsInN1YiI6IjY4Mjk3ZTAwOWFiODFhZjUwNWY1NTVkYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.TjVbPql5Eg1poQRSZYmAFUyeCkoojguMPja6DCNsTXE'
-                }
-            };
-
-            const {data, status} = await axios.get(url, options);
-            console.log(data)
-            console.log(status)
-
-            if (status === 200) {
-                setMovies(data.results)
-            }
-        } catch (e) {
-            console.log(e.message)
-        }
-    }
-
-    useEffect(() => {
-        getMovies()
-    }, []);
-
-  return (
-      <Container>
-          <Row>
-              {movies?.map((movie, index) => (
-                  <Col className={"mb-3"} key={index}>
-                      <Card style={{width: "18rem"}}>
-                          <Card.Img variant="top" src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}></Card.Img>
-                          <Card.Body>
-                              <Card.Title>{movie.original_title.slice(0, 10)}</Card.Title>
-                              <Card.Text>{movie.overview.slice(0, 100)}</Card.Text>
-                              <Card.Text>
-                                  출시일 :
-                                  {movie.release_date}
-                              </Card.Text>
-                          </Card.Body>
-                          <Button variant="primary">Go somewhere</Button>
-                      </Card>
-                  </Col>
-              ))}
-          </Row>
-      </Container>
-  )
+    return (
+        <>
+            <NavMenus/>
+            <Tvs/>
+        </>
+    )
 }
 
 export default App
