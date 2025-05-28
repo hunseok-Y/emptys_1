22fadeaddd80207281e32b9c4e906021643fc261
yanghoonseok <gnstjr5911@gmail.com>
2025-05-28 17:19:20 +0900

    axios 설치 및 사용 방법

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,46 @@
 import './App.css'
 import {useEffect, useState} from "react";
+import axios from "axios";
 
 function App() {
 
-    const [name, setName] = useState('Mr. Yang')
+    const [movies, setMovies] = useState([]);
 
-    const data = [
-        {
-            "userId": 1,
-            "id": 1,
-            "title": "delectus aut autem",
-            "completed": false
-        },
-        {
-            "userId": 1,
-            "id": 2,
-            "title": "quis ut nam facilis et officia qui",
-            "completed": false
-        },
-        {
-            "userId": 1,
-            "id": 3,
-            "title": "fugiat veniam minus",
-            "completed": false
-        },
-        {
-            "userId": 1,
-            "id": 4,
-            "title": "et porro tempora",
-            "completed": true
-        },
-        {
-            "userId": 1,
-            "id": 5,
-            "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
-            "completed": false
-        },
-    ]
+    const getMovies = async () => {
+        try {
+            const url = 'https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1';
+            const options = {
+                method: 'GET',
+                headers: {
+                    accept: 'application/json',
+                    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5Y2NlMjljMjc2MzZmODY0OGM0YzlmNmMyNGQyYTNhOCIsIm5iZiI6MTc0NzU0OTY5Ni41NjMsInN1YiI6IjY4Mjk3ZTAwOWFiODFhZjUwNWY1NTVkYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.TjVbPql5Eg1poQRSZYmAFUyeCkoojguMPja6DCNsTXE'
+                }
+            };
 
-    const changeName = () => {
-        setName("HoonSeok")
+            const {data, status} = await axios.get(url, options);
+            console.log(data)
+            console.log(status)
+
+            if (status === 200) {
+                setMovies(data.results)
+            }
+        } catch (e) {
+            console.log(e.message)
+        }
     }
 
-    const [movies, setMovies] = useState([])
-
     useEffect(() => {
-        setMovies(data)
+        getMovies()
     }, []);
 
   return (
       <>
-          <h1>Hello World</h1>
-          <h2>{name}</h2>
+          <h1>{movies.length}</h1>
           {movies?.map((movie, index) => (
               <div key={index}>
-                  <h1>{movie.title}</h1>
-                  <h2>{movie.completed ? "완료" : "완려되지 않음"}</h2>
+                  <h1>{movie.overview}</h1>
               </div>
           ))}
-          <button onClick={changeName}>change</button>
       </>
   )
 }
