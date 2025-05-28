de8df9695e3de296e14cbc3a108a3039ddaf77e5
yanghoonseok <gnstjr5911@gmail.com>
2025-05-28 16:43:09 +0900

    movie test

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,70 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {useState} from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+
+    const [name, setName] = useState('Mr. Yang')
+    const [age, setAge] = useState(0)
+    const [movieData, setMovieData] = useState({
+        "userId": 1,
+        "id": 1,
+        "title": "delectus aut autem",
+        "completed": false
+    })
+    const [movies, setMovies] = useState([
+        {
+            "userId": 1,
+            "id": 1,
+            "title": "delectus aut autem",
+            "completed": false
+        },
+        {
+            "userId": 1,
+            "id": 2,
+            "title": "quis ut nam facilis et officia qui",
+            "completed": false
+        },
+        {
+            "userId": 1,
+            "id": 3,
+            "title": "fugiat veniam minus",
+            "completed": false
+        },
+        {
+            "userId": 1,
+            "id": 4,
+            "title": "et porro tempora",
+            "completed": true
+        },
+        {
+            "userId": 1,
+            "id": 5,
+            "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+            "completed": false
+        },
+    ])
+
+    const changeName = () => {
+        setName("HoonSeok")
+        setAge(31)
+    }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <>
+          <h1>Hello World</h1>
+          <h3>{age}</h3>
+          <h2>{name}</h2>
+          {movies?.map((movie, index) => (
+              <div key={index}>
+                  <h1>{movie.title}</h1>
+                  <h2>{movie.completed ? "완료" : "완려되지 않음"}</h2>
+              </div>
+          ))}
+
+          <h1>{movieData.completed.toString()}</h1>
+
+          <button onClick={changeName}>change</button>
+      </>
   )
 }
 
